1e7fa3a686a68ad6bc5ae3b9e90975258ad7de68
Tiyasbiswas <tiyasbiswas17@gmail.com>
2022-07-27 15:08:42 +0200

    besic functionality

---
+++ app.js
@@ -0,0 +1,34 @@
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
+import authRouter from "./routes/auth.js";
+import connectToMongo from "./models/index.js";
+import cookieParser from "cookie-parser"
+
+if (!process.env.PORT) {
+  console.log("please provide PORT number and try again");
+  process.exit();
+}
+if (!process.env.SECRET) {
+  console.log("please provide SECRET and try again");
+  process.exit();
+}
+
+connectToMongo().then((connection) => {
+  const app = express();
+  app.use(express.json());
+  app.use(
+    cors({
+      origin: ["http://localhost:3000"],
+      methods: ["GET", "POST"],
+      credentials: true,
+    })
+  );
+  app.use(cookieParser());
+  
+  app.use("/auth", authRouter);
+
+  app.listen(process.env.PORT, () =>
+    console.log("listening on " + process.env.PORT)
+  );
+});
