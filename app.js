c589961ca7c9a7dff3a17865c06c387b63910a9d
Tiyasbiswas <tiyasbiswas17@gmail.com>
2022-08-10 11:31:01 +0200

    Chat functionality Added

--- app.js
+++ app.js
@@ -47,19 +47,25 @@ connectToMongo().then((connection) => {
 
   io.on("connection", (socket) => {
     console.log(`User Connected: ${socket.id}`);
+
+    socket.on('message', ({name, message }) => {
+      io.emit('message', { name, message })
+    })
   
-    socket.on("join_room", (data) => {
-      socket.join(data);
-      console.log(`User with ID: ${socket.id} joined room: ${data}`);
-    });
+    // socket.on("join_room", (data) => {
+    //   socket.join(data);
+    //   // console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    // });
   
-    socket.on("send_message", (data) => {
-      socket.to(data.room).emit("receive_message", data);
-    });
+    // socket.on("send_message", (data) => {
+    //   socket.to(data.room).emit("receive_message", data);
+    // });
   
-    socket.on("disconnect", () => {
-      console.log("User Disconnected", socket.id);
-    });
+    // socket.on("disconnect", () => {
+    //   console.log("User Disconnected", socket.id);
+    // });
+
+
   
   });
   
