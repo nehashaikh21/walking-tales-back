e848472ccadde5973573331da8ebd75a6e6e1659
Neha <nehashaikh470@gmail.com>
2023-04-26 17:52:00 +0200

    minor changes

--- app.js
+++ app.js
@@ -39,6 +39,19 @@ connectToMongo().then((connection) => {
     cors: {
       origin: [process.env.CLIENT],
       methods: ["GET", "POST"],
+      credentials: true,
+    },
+  });
+
+  io = require("socket.io")(server, {
+    handlePreflightRequest: (req, res) => {
+      const headers = {
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+        "Access-Control-Allow-Credentials": true,
+      };
+      res.writeHead(200, headers);
+      res.end();
     },
   });
 
